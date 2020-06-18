d98fbd6908b67dcdc8a264f697a221aa525a8717
Dara Danett Catú Oliva <dara.catu@hotmail.com>
2020-06-18 11:54:25 -0600

    Cambios

--- FrameworkMVC/templates_c/ec5658664500794492fba8dafbc763321517c10c_0.file.Default.tpl.php
+++ FrameworkMVC/templates_c/ec5658664500794492fba8dafbc763321517c10c_0.file.Default.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-06-17 01:37:54
+/* Smarty version 3.1.34-dev-7, created on 2020-06-18 19:53:48
   from 'C:\xampp\htdocs\DaraC\Framework\FrameworkMVC\templates\Default.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5ee957d204fa68_22347335',
+  'unifunc' => 'content_5eebaa2c41a216_05856909',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ec5658664500794492fba8dafbc763321517c10c' => 
     array (
       0 => 'C:\\xampp\\htdocs\\DaraC\\Framework\\FrameworkMVC\\templates\\Default.tpl',
-      1 => 1592350618,
+      1 => 1592502826,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:Cabecera/Footer.tpl' => 1,
   ),
 ),false)) {
-function content_5ee957d204fa68_22347335 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5eebaa2c41a216_05856909 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
   <?php $_smarty_tpl->_subTemplateRender("file:Cabecera/Header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
